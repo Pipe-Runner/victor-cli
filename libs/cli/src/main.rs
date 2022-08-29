c4ae94f7187d448cff58e6cc5d9fa5845d2a84ae
Pipe-Runner <piperunner.in@gmail.com>
2022-08-29 02:22:05 +0200

    Added basis generation methods

--- libs/cli/src/main.rs
+++ libs/cli/src/main.rs
@@ -18,7 +18,7 @@ fn convert_string_to_vector(input: &str) -> Vector {
 fn main() {
     println!("Welcome to Victor: Not so friendly Vector Processor\n");
 
-    println!("Please select an option:\n 1. Add two vectors\n 2. Subtract two vectors\n 3. Multiply a scalar to a vector\n 5. Exit");
+    println!("Please select an option:\n1. Add two vectors\n2. Subtract two vectors\n3. Multiply a scalar to a vector\n4. Angle between two vectors\n5. Dot product of two vectors\n6. Cross product of two 3D vectors\n7. Generate 3D basis from one vector\n8. Generate 3D basis from two vectors\n9. Exit");
     let operation: i32 = read_line().trim().parse::<i32>().unwrap();
 
     match operation {
@@ -33,7 +33,7 @@ fn main() {
             let v2 = convert_string_to_vector(&input);
 
             let result = v1.add(v2);
-            println!("\nThe result is:\n {}", result);
+            println!("\nThe result is:\n{}", result);
         }
         2 => {
             let mut input: String;
@@ -46,11 +46,11 @@ fn main() {
             let v2 = convert_string_to_vector(&input);
 
             let result = v1.sub(v2);
-            println!("\nThe result is:\n {}", result);
+            println!("\nThe result is:\n{}", result);
         }
         3 => {
             let mut input: String;
-            println!("Please enter the first vector: (single space separated numbers)");
+            println!("Please enter a first vector: (single space separated numbers)");
             input = read_line();
             let v1 = convert_string_to_vector(&input);
 
@@ -59,7 +59,68 @@ fn main() {
             let k = input.parse::<f32>().unwrap();
 
             let result = v1.scalar_mul(k);
-            println!("\nThe result is:\n {}", result);
+            println!("\nThe result is:\n{}", result);
+        }
+        4 => {
+            let mut input: String;
+            println!("Please enter the first vector: (single space separated numbers)");
+            input = read_line();
+            let v1 = convert_string_to_vector(&input);
+
+            println!("Please enter the second vector: (single space separated numbers)");
+            input = read_line();
+            let v2 = convert_string_to_vector(&input);
+
+            let result = v1.find_angle_between(&v2);
+            println!("\nThe result is:\n{} rad(s)", result);
+        }
+        5 => {
+            let mut input: String;
+            println!("Please enter the first vector: (single space separated numbers)");
+            input = read_line();
+            let v1 = convert_string_to_vector(&input);
+
+            println!("Please enter the second vector: (single space separated numbers)");
+            input = read_line();
+            let v2 = convert_string_to_vector(&input);
+
+            let result = v1.dot(&v2);
+            println!("\nThe result is:\n{}", result);
+        }
+        6 => {
+            let mut input: String;
+            println!("Please enter the first vector: (single space separated three numbers)");
+            input = read_line();
+            let v1 = convert_string_to_vector(&input);
+
+            println!("Please enter the second vector: (single space separated three numbers)");
+            input = read_line();
+            let v2 = convert_string_to_vector(&input);
+
+            let result = v1.cross(&v2);
+            println!("\nThe result is:\n{}", result);
+        }
+        7 => {
+            let input: String;
+            println!("Please enter a vector: (single space separated three numbers)");
+            input = read_line();
+            let v = convert_string_to_vector(&input);
+
+            let result = Vector::find_basis_with_one(&v);
+            println!("\nThe result is:\n{}\n{}\n{}", result.0, result.1, result.2);
+        }
+        8 => {
+            let mut input: String;
+            println!("Please enter the first vector: (single space separated three numbers)");
+            input = read_line();
+            let v1 = convert_string_to_vector(&input);
+
+            println!("Please enter the second vector: (single space separated three numbers)");
+            input = read_line();
+            let v2 = convert_string_to_vector(&input);
+
+            let result = Vector::find_basis_with_two(&v1, &v2);
+            println!("\nThe result is:\n{}\n{}\n{}", result.0, result.1, result.2);
         }
         _ => {
             println!("Goodbye");
--- libs/vector-processor/src/vector.rs
+++ libs/vector-processor/src/vector.rs
@@ -75,6 +75,85 @@ impl Vector {
 
         Self { data: new_data }
     }
+
+    pub fn dot(&self, v1: &Self) -> f32 {
+        let min_dim = self.len().min(v1.len());
+
+        let mut dot_product: f32 = 0.0;
+        for i in 0..min_dim {
+            dot_product += self.data[i] * v1.data[i];
+        }
+        dot_product
+    }
+
+    // Only works for 3D or less
+    pub fn cross(&self, v1: &Self) -> Self {
+        assert!(
+            self.len() <= 3 && v1.len() <= 3,
+            "Cross product is only defined for 3D vectors"
+        );
+
+        let mut a = vec![0.0; 3];
+        let mut b = vec![0.0; 3];
+
+        for i in 0..self.len() {
+            a[i] = self.data[i];
+        }
+
+        for i in 0..v1.len() {
+            b[i] = v1.data[i];
+        }
+
+        let mut new_data: Vec<f32> = vec![0.0; 3];
+        new_data[0] = a[1] * b[2] - a[2] * b[1];
+        new_data[1] = a[2] * b[0] - a[0] * b[2];
+        new_data[2] = a[0] * b[1] - a[1] * b[0];
+        Self { data: new_data }
+    }
+
+    pub fn norm(&self) -> f32 {
+        let mut sum: f32 = 0.0;
+        for i in &self.data {
+            sum += i.powi(2);
+        }
+        sum.sqrt()
+    }
+
+    // Answer is in radians
+    pub fn find_angle_between(&self, v1: &Self) -> f32 {
+        (self.dot(v1) / (self.norm() * v1.norm())).acos()
+    }
+
+    // Returns a 3D basis for a given vector.
+    pub fn find_basis_with_one(v: &Self) -> (Self, Self, Self) {
+        let temp = v.add(Vector::new(vec![1.0, 1.0, 1.0]));
+
+        let b = v.cross(&temp);
+        let c = b.cross(&v);
+
+        (
+            v.scalar_mul(1.0 / v.norm()),
+            b.scalar_mul(1.0 / b.norm()),
+            c.scalar_mul(1.0 / c.norm()),
+        )
+    }
+
+    // Returns a 3D basis for a given vector.
+    pub fn find_basis_with_two(v1: &Self, v2: &Self) -> (Self, Self, Self) {
+        if v1.dot(v2) == 1.0 {
+            // vectors are parallel
+            return Self::find_basis_with_one(v1);
+        }
+
+        let a = v1.cross(v2);
+        let b = v1.cross(&a);
+
+        (
+            v1.scalar_mul(1.0 / v1.norm()),
+            a.scalar_mul(1.0 / a.norm()),
+            b.scalar_mul(1.0 / b.norm()),
+        )
+    }
 }
 
 impl fmt::Display for Vector {
@@ -93,6 +172,13 @@ impl fmt::Display for Vector {
 #[cfg(test)]
 mod test {
     use super::*;
+
+    #[test]
+    fn find_angle_between() {
+        let v1 = Vector::new(vec![1.0, 0.0, 0.0]);
+        let v2 = Vector::new(vec![-1.0, 0.0, 0.0]);
+        assert_eq!(v1.find_angle_between(&v2), std::f32::consts::PI);
+    }
 
     #[test]
     fn add() {
@@ -129,7 +215,34 @@ mod test {
     #[test]
     fn scalar_mul() {
         let v1 = Vector::new(vec![1.0, 2.0, 3.0, 20.0]);
-        let v2 = v1.scalar_mul(2.0);
-        assert_eq!(v2.data, vec![2.0, 4.0, 6.0, 40.0]);
+        assert_eq!(v1.scalar_mul(2.0).data, vec![2.0, 4.0, 6.0, 40.0]);
+    }
+
+    #[test]
+    fn dot() {
+        let v1 = Vector::new(vec![1.0, 2.0, 3.0]);
+        let v2 = Vector::new(vec![1.0, 2.0, 3.0]);
+        assert_eq!(v1.dot(&v2), 14.0);
+    }
+
+    #[test]
+    fn dot_unequal() {
+        let v1 = Vector::new(vec![1.0, 2.0, 3.0, 20.0]);
+        let v2 = Vector::new(vec![1.0, 2.0]);
+        assert_eq!(v1.dot(&v2), 5.0);
+    }
+
+    #[test]
+    fn cross() {
+        let v1 = Vector::new(vec![1.0, 2.0, 3.0]);
+        let v2 = Vector::new(vec![3.0, 2.0, 1.0]);
+        assert_eq!(v1.cross(&v2).data, vec![-4.0, 8.0, -4.0]);
+    }
+
+    #[test]
+    fn cross_unequal() {
+        let v1 = Vector::new(vec![1.0, 2.0, 3.0]);
+        let v2 = Vector::new(vec![3.0, 2.0]);
+        assert_eq!(v1.cross(&v2).data, vec![-6.0, 9.0, -4.0]);
     }
 }
